7980ad5178240e2a8b846d760bdab0675302464d
Dario LaRosa <dariolarosa@hotmail.com>
2025-12-26 11:42:44 +0100

    refactor: rinomina il progetto da agenda_frontend_2 a agenda_frontend

--- agenda_frontend_2/test/widget_test.dart
+++ agenda_frontend_2/test/widget_test.dart
@@ -1,15 +1,10 @@
-import 'package:flutter_test/flutter_test.dart';
+import 'package:agenda_frontend/app/app.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-
-import 'package:agenda_frontend_2/app/app.dart';
+import 'package:flutter_test/flutter_test.dart';
 
 void main() {
   testWidgets('App builds without crashing', (WidgetTester tester) async {
-    await tester.pumpWidget(
-      const ProviderScope(
-        child: App(),
-      ),
-    );
+    await tester.pumpWidget(const ProviderScope(child: App()));
 
     // Verifica che l'app si avvii correttamente
     expect(find.byType(App), findsOneWidget);
