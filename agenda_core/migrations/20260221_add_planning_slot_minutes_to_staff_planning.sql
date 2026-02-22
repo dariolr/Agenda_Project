0d7f9baecea4d91a6fa5828d11601b9acccc56fd
dariolarosa <dariolarosa@hotmail.com>
2026-02-22 15:23:31 +0700

    cscdsdsd

--- agenda_core/migrations/20260221_add_planning_slot_minutes_to_staff_planning.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE `staff_planning`
-  ADD COLUMN `planning_slot_minutes` tinyint UNSIGNED NOT NULL DEFAULT 15
-  COMMENT 'Passo (in minuti) usato per generare il planning'
-  AFTER `type`;
