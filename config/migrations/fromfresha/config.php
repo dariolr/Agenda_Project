169315846591f0fef8a2ceb55dcbcfa3c87e1337
dariolarosa <dariolarosa@hotmail.com>
2026-06-08 15:05:35 +0200

    xcxcxcxcx

--- config/migrations/fromfresha/config.php
+++ config/migrations/fromfresha/config.php
@@ -1,12 +1,12 @@
 <?php
 const FRESHA_CONFIG = array (
-  'business_id' => 21,
-  'location_id' => 26,
+  'business_id' => 25,
+  'location_id' => 28,
   'csv_clients' => 'export_customer_list.csv',
   'csv_services' => 'export_service_list.csv',
   'csv_staff' => '',
   'skip_blocked_clients' => false,
-  'dry_run' => false,
+  'dry_run' => true,
   // Elimina dati esistenti prima di importare (per rieseguire migrazione pulita)
   'clear_existing_data' => true,
 );
